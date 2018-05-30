54722fef247c6ac7cd581b02400d0f1ba3c6de68
Ganesh <ganesh.alkurn@gmail.com>
2018-05-30 10:55:11 +0530

    Update

--- Upload.php
+++ Upload.php
@@ -118,8 +118,9 @@ class Upload extends Model
         if (! is_dir($path)) {
             mkdir($path, 0777, true);
         }
-
-        copy($options['url'], $path . $name);
+        if(file_exists($options['url'])){
+            copy($options['url'], $path . $name);
+        }
         return $this->getBaseName( $baseName ) . $name;
     }
 
